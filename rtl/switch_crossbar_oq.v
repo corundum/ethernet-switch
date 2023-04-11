aa9320b5d69c50eab29a70696a373b1de9fa6b3c
Carlos Megías <narg@ugr.es>
2023-04-11 11:15:10 +0200

    Add OQ switch implementation

---
+++ rtl/switch_crossbar_oq.v
@@ -0,0 +1,186 @@
+/*
+
+Copyright (c) 2023 Corundum organization
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+module switch_crossbar_oq #
+(
+    // AXI streaming interface parameters
+    // Width of data bus in bits
+    parameter AXIS_DATA_WIDTH = 64,
+    // Width of keep signal in bits
+    parameter AXIS_KEEP_WIDTH = AXIS_DATA_WIDTH/8,
+    // Enable id signal propagation
+    parameter AXIS_ID_ENABLE = 1,
+    // Width of id signal in bits
+    parameter AXIS_ID_WIDTH = 8,
+    // Enable user signal propagation
+    parameter AXIS_USER_ENABLE = 1,
+    // Width of user signal in bits
+    parameter AXIS_USER_WIDTH = 17,
+    
+    // Architectural parameters
+    // Number of ports (radix of the switch)
+    parameter RADIX = 4,
+    // Enable dest signal propagation
+    parameter AXIS_DEST_ENABLE = 1,
+    // Width of dest signal in bits
+    parameter AXIS_DEST_WIDTH = RADIX
+)
+(
+    input  wire                                   clk,
+    input  wire                                   rst,
+    
+    /*
+     * AXI-Stream input
+     */
+    input  wire [RADIX*AXIS_DATA_WIDTH-1:0]       s_axis_tdata,
+    input  wire [RADIX*AXIS_DATA_WIDTH/8-1:0]     s_axis_tkeep,
+    input  wire [RADIX-1:0]                       s_axis_tvalid,
+    output wire [RADIX-1:0]                       s_axis_tready,
+    input  wire [RADIX-1:0]                       s_axis_tlast,
+    input  wire [RADIX*AXIS_ID_WIDTH-1:0]         s_axis_tid,
+    input  wire [RADIX*AXIS_DEST_WIDTH-1:0]       s_axis_tdest,
+    input  wire [RADIX*AXIS_USER_WIDTH-1:0]       s_axis_tuser,
+
+    /*
+     * AXI-Stream output
+     */
+    output wire [RADIX*AXIS_DATA_WIDTH-1:0]       m_axis_tdata,
+    output wire [RADIX*AXIS_DATA_WIDTH/8-1:0]     m_axis_tkeep,
+    output wire [RADIX-1:0]                       m_axis_tvalid,
+    input  wire [RADIX-1:0]                       m_axis_tready,
+    output wire [RADIX-1:0]                       m_axis_tlast,
+    output wire [RADIX*AXIS_ID_WIDTH-1:0]         m_axis_tid,
+    output wire [RADIX*AXIS_DEST_WIDTH-1:0]       m_axis_tdest,
+    output wire [RADIX*AXIS_USER_WIDTH-1:0]       m_axis_tuser
+);
+
+// auxiliar registers and wires
+reg [RADIX*RADIX-1:0]  s_arbiter_axis_tvalid = 0;
+reg [RADIX-1:0]  s_axis_tready_int = 0;
+wire [RADIX*RADIX-1:0] s_axis_tready_reg;
+
+// input arbiter selected based on tdest signal
+integer m, k, h, j;
+always @* begin
+    // check which input ports have something to transmit (s_axis_tvalid[k])
+    // then check individually which output port they need (s_axis_tdest[k][m])
+    s_arbiter_axis_tvalid = 0;
+    // output port loop
+    for (m = 0; m < RADIX; m = m + 1) begin
+        // input port loop
+        for (k = 0; k < RADIX; k = k + 1) begin
+            // which connections are valid
+            s_arbiter_axis_tvalid[m*RADIX+k] = s_axis_tvalid[k] & s_axis_tdest[m+k*RADIX];
+        end
+        // s_arbiter_axis_tvalid[m*RADIX +: RADIX] = s_axis_tvalid & s_arbiter_axis_tvalid[m*RADIX +: RADIX];
+    end
+
+    s_axis_tready_int = 0;
+    // input port loop
+    for (h = 0; h < RADIX; h = h + 1) begin
+        // output port loop
+        for (j = 0; j < RADIX; j = j + 1) begin
+            s_axis_tready_int[h] = s_axis_tready_int[h] | (s_axis_tready_reg[h+j*RADIX] & s_arbiter_axis_tvalid[h+j*RADIX]);
+        end
+    end    
+end
+
+assign s_axis_tready = s_axis_tready_int;
+
+// output wires
+wire [RADIX*AXIS_DATA_WIDTH-1:0]             m_axis_tdata_reg;
+wire [RADIX*AXIS_KEEP_WIDTH-1:0]             m_axis_tkeep_reg;
+wire [RADIX-1:0]                             m_axis_tvalid_reg;
+wire [RADIX-1:0]                             m_axis_tready_reg;
+wire [RADIX-1:0]                             m_axis_tlast_reg;
+wire [RADIX*AXIS_ID_WIDTH-1:0]               m_axis_tid_reg;
+wire [RADIX*AXIS_DEST_WIDTH-1:0]             m_axis_tdest_reg;
+wire [RADIX*AXIS_USER_WIDTH-1:0]             m_axis_tuser_reg;
+
+// Instantiation of one arbiter per output port
+generate
+    genvar n;
+    for (n = 0; n < RADIX; n = n + 1) begin : arbiters
+        axis_arb_mux #(
+            .S_COUNT(RADIX),
+            .DATA_WIDTH(AXIS_DATA_WIDTH),
+            .KEEP_ENABLE(1),
+            .KEEP_WIDTH(AXIS_KEEP_WIDTH),
+            .ID_ENABLE(AXIS_ID_ENABLE),                                                                 
+            .S_ID_WIDTH(AXIS_ID_WIDTH),                                                            
+            .M_ID_WIDTH(AXIS_ID_WIDTH),                                                            
+            .DEST_ENABLE(1),                                                               
+            .DEST_WIDTH(AXIS_DEST_WIDTH),                                                           
+            .USER_ENABLE(AXIS_USER_ENABLE),                                                            
+            .USER_WIDTH(AXIS_USER_WIDTH),      
+            .LAST_ENABLE(1),
+            .UPDATE_TID(0),       
+            .ARB_TYPE_ROUND_ROBIN(0),                                                                    
+            .ARB_LSB_HIGH_PRIORITY(1)        
+        )
+        axis_arb_mux_inst (
+            .clk(clk),
+            .rst(rst),
+
+            /*
+            * AXI-Stream input
+            */
+            .s_axis_tdata(s_axis_tdata),
+            .s_axis_tkeep(s_axis_tkeep),
+            .s_axis_tvalid(s_arbiter_axis_tvalid[n*RADIX +: RADIX]),
+            .s_axis_tready(s_axis_tready_reg[n*RADIX +: RADIX]),
+            .s_axis_tlast(s_axis_tlast),
+            .s_axis_tid(s_axis_tid),
+            .s_axis_tdest(s_axis_tdest),
+            .s_axis_tuser(s_axis_tuser),
+
+            /*
+            * AXI-Stream output
+            */
+            .m_axis_tdata(m_axis_tdata_reg[n*AXIS_DATA_WIDTH +: AXIS_DATA_WIDTH]),
+            .m_axis_tkeep(m_axis_tkeep_reg[n*AXIS_KEEP_WIDTH +: AXIS_KEEP_WIDTH]),
+            .m_axis_tvalid(m_axis_tvalid_reg[n]),
+            .m_axis_tready(m_axis_tready_reg[n]),
+            .m_axis_tlast(m_axis_tlast_reg[n]),
+            .m_axis_tid(m_axis_tid_reg[n*AXIS_ID_WIDTH +: AXIS_ID_WIDTH]),
+            .m_axis_tdest(m_axis_tdest_reg[n*AXIS_DEST_WIDTH +: AXIS_DEST_WIDTH]),
+            .m_axis_tuser(m_axis_tuser_reg[n*AXIS_USER_WIDTH +: AXIS_USER_WIDTH])
+        );
+    end
+endgenerate
+
+assign m_axis_tdata = m_axis_tdata_reg;
+assign m_axis_tkeep = m_axis_tkeep_reg;
+assign m_axis_tvalid = m_axis_tvalid_reg;
+assign m_axis_tready_reg = m_axis_tready;
+assign m_axis_tlast = m_axis_tlast_reg;
+assign m_axis_tid = m_axis_tid_reg;
+assign m_axis_tdest = m_axis_tdest_reg;
+assign m_axis_tuser = m_axis_tuser_reg;
+
+endmodule
---
+++ rtl/switch_oq_wrap.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+"""
+Generates an AXI Stream switch wrapper with the specified number of ports
+"""
+
+import argparse
+from jinja2 import Template
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__.strip())
+    parser.add_argument('-p', '--ports',  type=int, default=[4], nargs='+', help="number of ports")
+    parser.add_argument('-n', '--name',   type=str, help="module name")
+    parser.add_argument('-o', '--output', type=str, help="output file name")
+
+    args = parser.parse_args()
+
+    try:
+        generate(**args.__dict__)
+    except IOError as ex:
+        print(ex)
+        exit(1)
+
+
+def generate(ports=4, name=None, output=None):
+    if type(ports) is int:
+        m = n = ports
+    elif len(ports) == 1:
+        m = n = ports[0]
+    else:
+        m, n = ports
+
+    if name is None:
+        name = "switch_oq_wrap_{0}x{1}".format(m, n)
+
+    if output is None:
+        output = name + ".v"
+
+    print("Generating {0}x{1} port AXI stream switch wrapper {2}...".format(m, n, name))
+
+    cm = (m-1).bit_length()
+    cn = (n-1).bit_length()
+
+    t = Template(u"""/*
+
+Copyright (c) 2023 Corundum organization
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+// Language: Verilog 2001
+
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+ * AXI4-Stream {{m}}x{{n}} switch (wrapper)
+ */
+module {{name}} #
+(
+    // AXI streaming interface parameters
+    // Width of data bus in bits
+    parameter AXIS_DATA_WIDTH = 64,
+    // Width of keep signal in bits
+    parameter AXIS_KEEP_WIDTH = AXIS_DATA_WIDTH/8,
+    // Enable id signal propagation
+    parameter AXIS_ID_ENABLE = 1,
+    // Width of id signal in bits
+    parameter AXIS_ID_WIDTH = 8,
+    // Enable user signal propagation
+    parameter AXIS_USER_ENABLE = 1,
+    // Width of user signal in bits
+    parameter AXIS_USER_WIDTH = 17,
+    
+    // Architectural parameters
+    // Number of ports (radix of the switch)
+    parameter RADIX = 4,
+    // Enable dest signal propagation
+    parameter AXIS_DEST_ENABLE = 1,
+    // Width of dest signal in bits
+    parameter AXIS_DEST_WIDTH = RADIX
+)
+(
+    input  wire                                   clk,
+    input  wire                                   rst,
+
+    /*
+     * Speed up clk and reset input
+     */
+    input  wire                                   clk_su,
+    input  wire                                   rst_su,
+
+    /*
+     * AXI Stream inputs
+     */
+{%- for p in range(m) %}
+    input  wire [AXIS_DATA_WIDTH-1:0]  s{{'%02d'%p}}_axis_tdata,
+    input  wire [AXIS_KEEP_WIDTH-1:0]  s{{'%02d'%p}}_axis_tkeep,
+    input  wire                        s{{'%02d'%p}}_axis_tvalid,
+    output wire                        s{{'%02d'%p}}_axis_tready,
+    input  wire                        s{{'%02d'%p}}_axis_tlast,
+    input  wire [AXIS_ID_WIDTH-1:0]    s{{'%02d'%p}}_axis_tid,
+    input  wire [AXIS_DEST_WIDTH-1:0]  s{{'%02d'%p}}_axis_tdest,
+    input  wire [AXIS_USER_WIDTH-1:0]  s{{'%02d'%p}}_axis_tuser,
+{% endfor %}
+    /*
+     * AXI Stream outputs
+     */
+{%- for p in range(n) %}
+    output wire [AXIS_DATA_WIDTH-1:0]  m{{'%02d'%p}}_axis_tdata,
+    output wire [AXIS_KEEP_WIDTH-1:0]  m{{'%02d'%p}}_axis_tkeep,
+    output wire                        m{{'%02d'%p}}_axis_tvalid,
+    input  wire                        m{{'%02d'%p}}_axis_tready,
+    output wire                        m{{'%02d'%p}}_axis_tlast,
+    output wire [AXIS_ID_WIDTH-1:0]    m{{'%02d'%p}}_axis_tid,
+    output wire [AXIS_DEST_WIDTH-1:0]  m{{'%02d'%p}}_axis_tdest,
+    output wire [AXIS_USER_WIDTH-1:0]  m{{'%02d'%p}}_axis_tuser{% if not loop.last %},{% endif %}
+{% endfor -%}
+);
+
+// parameter sizing helpers
+function [AXIS_DEST_WIDTH-1:0] w_dw(input [AXIS_DEST_WIDTH-1:0] val);
+    w_dw = val;
+endfunction
+
+function [{{m-1}}:0] w_s(input [{{m-1}}:0] val);
+    w_s = val;
+endfunction
+
+switch_oq #(
+    .AXIS_DATA_WIDTH(AXIS_DATA_WIDTH),
+    .AXIS_KEEP_WIDTH(AXIS_KEEP_WIDTH),
+    .AXIS_ID_ENABLE(AXIS_ID_ENABLE),
+    .AXIS_ID_WIDTH(AXIS_ID_WIDTH),
+    .AXIS_DEST_ENABLE(AXIS_DEST_ENABLE),
+    .AXIS_DEST_WIDTH(AXIS_DEST_WIDTH),
+    .AXIS_USER_ENABLE(AXIS_USER_ENABLE),
+    .AXIS_USER_WIDTH(AXIS_USER_WIDTH),
+    .RADIX(RADIX)
+)
+switch_inst (
+    .clk(clk),
+    .rst(rst),
+    // Speedup clock and reset
+    .clk_su(clk_su),
+    .rst_su(rst_su),
+    // AXI inputs
+    .s_axis_tdata({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tdata{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tkeep({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tkeep{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tvalid({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tvalid{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tready({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tready{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tlast({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tlast{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tid({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tid{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tdest({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tdest{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .s_axis_tuser({ {% for p in range(m-1,-1,-1) %}s{{'%02d'%p}}_axis_tuser{% if not loop.last %}, {% endif %}{% endfor %} }),
+    // AXI outputs
+    .m_axis_tdata({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tdata{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tkeep({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tkeep{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tvalid({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tvalid{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tready({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tready{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tlast({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tlast{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tid({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tid{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tdest({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tdest{% if not loop.last %}, {% endif %}{% endfor %} }),
+    .m_axis_tuser({ {% for p in range(n-1,-1,-1) %}m{{'%02d'%p}}_axis_tuser{% if not loop.last %}, {% endif %}{% endfor %} })
+);
+
+endmodule
+
+`resetall
+
+""")
+
+    print(f"Writing file '{output}'...")
+
+    with open(output, 'w') as f:
+        f.write(t.render(
+            m=m,
+            n=n,
+            cm=cm,
+            cn=cn,
+            name=name
+        ))
+        f.flush()
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
